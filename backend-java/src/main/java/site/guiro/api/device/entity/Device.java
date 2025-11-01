a450ca60321e7aea81835b390235961f3f94bee4
sseobi16 <jseobi3951@naver.com>
2025-11-01 21:06:00 +0900

    feat: Entity 설정 #1

---
+++ backend-java/src/main/java/site/guiro/api/device/entity/Device.java
@@ -0,0 +1,47 @@
+package site.guiro.api.device.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import site.guiro.api.guide.entity.CaptureImage;
+import site.guiro.api.guide.entity.TourSession;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "device")
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id") // BIGINT AUTO_INCREMENT
+    private Long id;
+
+    @Column(name = "device_id", length = 44, nullable = false) // SHA-256(Base64/Url)
+    private String deviceId;
+
+    @Column(name = "platform", length = 16) // 'ANDROID' | 'IOS'
+    @Enumerated(EnumType.STRING)
+    private  Platform platform;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    @Column(name = "last_seen_at")
+    private Instant lastSeenAt;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "device", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CaptureImage> captureImageList = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "device", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<TourSession> tourSessionList = new ArrayList<>();
+
+}
---
+++ backend-java/src/main/java/site/guiro/api/guide/entity/ExtractedSlot.java
@@ -0,0 +1,34 @@
+package site.guiro.api.guide.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import site.guiro.api.poi.entity.PoiCache;
+
+import java.time.Instant;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "extracted_slot")
+public class ExtractedSlot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "slot_id")
+    private Long slotId;
+
+    @Column(name = "model_name", length = 100, nullable = false)
+    private String modelName; // 'koelectra-bigru' 등
+
+    @Column(name = "slot_json", columnDefinition = "json", nullable = false)
+    private String slotJson; // JSON 문자열
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "poi_key", nullable = false)
+    private PoiCache poiKey;
+}
