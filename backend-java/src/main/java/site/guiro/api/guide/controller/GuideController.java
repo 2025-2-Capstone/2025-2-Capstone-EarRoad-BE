c0e6aa2a70577a8f3b6d64649154e0bd49c4339f
sseobi16 <jseobi3951@naver.com>
2025-11-14 22:58:18 +0900

    feat: 가이드 세션 관리를 위한 초기 세팅 #7

---
+++ backend-java/src/main/java/site/guiro/api/guide/controller/GuideController.java
@@ -0,0 +1,11 @@
+package site.guiro.api.guide.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/guide")
+@RequiredArgsConstructor
+public class GuideController {
+}
