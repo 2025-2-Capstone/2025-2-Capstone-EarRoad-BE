9ce9ae8428d89b41b289d3b45f7abc1c4f9c4803
sseobi16 <jseobi3951@naver.com>
2025-11-18 02:07:49 +0900

    feat: 스웨거 연결 설정 및 DB 수정 #11

--- backend-java/src/main/java/site/guiro/api/config/security/SecurityConfig.java
+++ backend-java/src/main/java/site/guiro/api/config/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
